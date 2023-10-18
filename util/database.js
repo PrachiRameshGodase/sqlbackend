4ff6a2aaab0c98716ad28ef17325967cfbdebc77
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-10-18 23:39:13 +0530

    Retrieving data from databases

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const mysql=require('mysql2')
 
 const pool=mysql.createPool({
-    host:"localhost",
-    user:"'root",
-    database:"node-complete",
+    host:'localhost',
+    user:'root',
+    database:'node-complete',
     password:'Prachi@123'
 
 })
