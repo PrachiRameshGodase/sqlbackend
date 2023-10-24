4e69dc0f4291cb9217bafb452fe99de93ba324f1
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-10-24 16:32:27 +0530

    Design the User and Product relationship

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title:title,
     price:price,
     imageUrl:imageUrl,
-    description:description
-  }).then(result=>{
+    description:description,
+    
+  })
+  .then(result=>{
     // console.log(result)
     console.log("Created product")
     res.redirect('/admin/products');
@@ -34,7 +36,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
+  req.user.getProducts({where:{id:prodId}})
+ .then(products => {
+  const product=products[0]
     if (!product) {
       return res.redirect('/');
     }
@@ -55,7 +59,8 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId).then(product=>{
+  Product.findByPk(prodId)
+  .then(product=>{
     product.title=updatedTitle;
     product.price=updatedPrice;
     product.description=updatedDesc;
@@ -71,7 +76,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products => {
+  req.user.getProducts()
+  .then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
